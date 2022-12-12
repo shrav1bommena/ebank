12e66dfaa7b82616e6602355cb9a6dab455e1147
shravan <shrav1bommena@gmail.com>
2022-12-12 11:44:45 +0000

    added not found css

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,11 +1,16 @@
+import './index.css'
+
 const NotFound = () => (
-  <div>
+  <div className="not-found-container">
     <img
+      className="not-found-image"
       src="https://assets.ccbp.in/frontend/react-js/ebank-not-found-img.png"
       alt="not found"
     />
-    <h1>Page Not Found</h1>
-    <p>We are sorry, the page you requested could not be found.</p>
+    <h1 className="not-found-title">Page Not Found</h1>
+    <p className="not-found-desc">
+      We are sorry, the page you requested could not be found.
+    </p>
   </div>
 )
 
